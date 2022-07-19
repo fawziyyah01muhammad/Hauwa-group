47208caebc4ff803930a708e2d01a1ca19030ae8
fawziyyah01muhammad <mziyyah010@gmail.com>
2022-07-19 17:14:53 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
-document.querySelectorAll('input').forEach((input) =>{
-    input.addEventListener('click', (e) => {
-        alert(`you are editing the ${e.target.name} field!`)
-    });
-})
+// document.querySelectorAll('input').forEach((input) =>{
+//     input.addEventListener('click', (e) => {
+//         alert(`you are editing the ${e.target.name} field!`)
+//     });
+// })
 
-let require = document.querySelectorAll('input')
-let Submit = document.querySelector('.sign-up-container')
-if(require == ""){
-Submit.addEventListener('submit', (e) => {
-        e.preventDefault();
-        alert("Your submission was not successful!");
-    })
-}
+// let require = document.querySelectorAll('input')
+// let Submit = document.querySelector('.sign-up-container')
+// if(require == ""){
+// Submit.addEventListener('submit', (e) => {
+//         e.preventDefault();
+//         alert("Your submission was not successful!");
+//     })
+// }
+
+
+// --------------password visibility---------------
+
+const seePassword = document.querySelector('#togglePassword');
+
+const password = document.querySelector('#password');
+    
+seePassword.addEventListener('click', () =>{
+    const type = password.getAttribute('type') === 'password' ?
+    'text' : 'password';
+    password.setAttribute('type', type);
+
+this.classList.toggle('fa-eye-eye')
+
+});
+
+
+
+
+//------------------form validation-----------------
+
